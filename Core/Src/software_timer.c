263762f6a29ddc44c444ea447a9bd15421aea54e
DND318 <162724857+DND318@users.noreply.github.com>
2024-05-11 23:01:01 +0700

    Update software_timer.c

--- Core/Src/software_timer.c
+++ Core/Src/software_timer.c
@@ -6,14 +6,25 @@
  */
 #include "software_timer.h"
 #define TICK 10
+int timer0_flag =0;
 int timer1_flag =0;
 int timer2_flag =0;
 int timer3_flag =0;
 int timer4_flag =0;
+int timer0_counter =0;
 int timer1_counter =0;
 int timer2_counter =0;
 int timer3_counter =0;
 int timer4_counter =0;
+
+void setTimer0(int duration){
+	timer0_counter = duration/TICK;
+	timer0_flag =0;
+}
+void clearTimer0(){
+	timer0_counter=0;
+	timer0_flag=0;
+}
 
 void setTimer1(int duration){
 	timer1_counter = duration/TICK;
@@ -51,6 +62,13 @@ void clearTimer4(){
 	timer4_counter =0;
 }
 void timerRun(){
+	if (timer0_counter > 0){
+			timer0_counter--;
+			if(timer0_counter <=0){
+				timer0_flag = 1;
+			}
+		}
+
 	if (timer1_counter > 0){
 		timer1_counter--;
 		if(timer1_counter <=0){
