7d0ebfe2158343bb415146275e446726d2cd1a19
DND318 <162724857+DND318@users.noreply.github.com>
2024-05-11 23:05:12 +0700

    Update global.h

--- Core/Inc/global.h
+++ Core/Inc/global.h
@@ -8,24 +8,36 @@
 #ifndef INC_GLOBAL_H_
 #define INC_GLOBAL_H_
 
-#include "software_timer.h"
 #include "main.h"
+#include "software_timer.h"
+#include "software_timer.h"
 #include "button.h"
-
+#include "main.h"
+#include "fsm_normal.h"
 #define INIT		1
-#define AUTO_RED	2
-#define AUTO_YELLOW	3
-#define AUTO_GREEN	4
+#define RED_GREEN	2
+#define RED_YELLOW	3
+#define GREEN_RED	4
+#define YELLOW_RED	5
 
-#define MAN_RED = 12;
-#define MAN_GREEN = 13;
-#define MAN_YELLOW = 14;
+
+#define MAN_RED 	12
+#define MAN_GREEN	13
+#define MAN_YELLOW	14
+
+extern int NORMAL_RED;
+extern int NORMAL_GREEN;
+extern int NORMAL_YELLOW;
+
+extern int LedStatus;
+extern int mode;
+
+extern int count1 ;
+extern int count2 ;
+
+extern int countMan1;
+extern int countMan2;
 
 extern int status;
-extern int count1;
-extern int count2;
-extern int LedStatus;
-extern int manualCount1;
-extern int manualCount2;
 
 #endif /* INC_GLOBAL_H_ */
