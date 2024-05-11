710c351277f7d09a553f7ccff40ea601d01e8811
DND318 <162724857+DND318@users.noreply.github.com>
2024-05-11 23:06:36 +0700

    Update and rename 7SEG_DISPLAY.h to modifyLed.h

---
+++ Core/Inc/modifyLed.h
@@ -0,0 +1,17 @@
+/*
+ * modifyLed.h
+ *
+ *  Created on: May 11, 2024
+ *      Author: Dell
+ */
+
+#ifndef INC_MODIFYLED_H_
+#define INC_MODIFYLED_H_
+
+#include "global.h"
+
+
+
+void fsm_modify_run();
+
+#endif /* INC_MODIFYLED_H_ */
